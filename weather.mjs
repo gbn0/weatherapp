a348f1f3d97bc3eeda7366a4059b7823428c6ee5
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-04-27 14:04:15 -0300

    v1

---
+++ weather.mjs
@@ -0,0 +1,61 @@
+const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=-30.06&longitude=-51.17&hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum&current_weather=true&timeformat=unixtime&forecast_days=8&timezone=America%2FSao_Paulo`;
+
+export function getWeather() {
+  return fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
+      };
+    });
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+  const {
+    temperature: currentTemp,
+    weathercode: iconCode,
+    windspeed: windSpeed,
+  } = current_weather;
+
+  const {
+    precipitation_sum: [precip],
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+  } = daily;
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
+}
+
+function parseDailyWeather({ daily }) {
+  return daily.time.map((time, index) => {
+    return {
+      timeStamp: time * 1000,
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+      minTemp: Math.round(daily.temperature_2m_min[index]),
+      iconCode: daily.weathercode[index],
+    };
+  });
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timeStamp: time * 1000,
+        temp: Math.round(hourly.temperature_2m[index]),
+        appTemp: Math.round(hourly.apparent_temperature[index]),
+        iconCode: hourly.weathercode[index],
+      };
+    })
+    .filter(({ timeStamp }) => timeStamp >= current_weather.time * 1000);
+}
