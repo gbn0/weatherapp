e73671fad8fcdc8b0772664fec30c4ff59f6e521
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-04-28 17:02:43 -0300

    carrega dias

--- renderer.mjs
+++ renderer.mjs
@@ -1,5 +1,8 @@
 let weekDays = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
 
+const template = document.querySelector("#box-template");
+const boxes = document.querySelector("#boxes");
+
 const iconMap = new Map();
 
 function addMapping(values, icon) {
@@ -13,6 +16,12 @@ export function renderCurrentWeather({ current }) {}
 export function renderDailyWeather({ daily }) {
   daily.forEach((day) => {
     let date = new Date(day.timeStamp).getDay();
+    const div = template.content.cloneNode(true);
+    div.querySelector("[week-day]").textContent = weekDays[date];
+
+    div.querySelector("[max-day]").textContent = day.maxTemp;
+    div.querySelector("[min-day]").textContent = day.minTemp;
+    boxes.appendChild(div);
 
     console.log(weekDays[date]);
   });
