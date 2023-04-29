b66bf61bcce888583d20c3195193aa8eef4688ce
Gabriel Saldanha <gabrielsdnegre@gmail.com>
2023-04-29 02:33:31 -0300

    animação de hover e colocar o icone certo em cada dia

--- renderer.mjs
+++ renderer.mjs
@@ -5,6 +5,7 @@ const boxes = document.querySelector("#boxes");
 const mainTemp = document.querySelector("#maintemp");
 
 const iconMap = new Map();
+addMapping([1], "sun-icon.svg");
 
 function addMapping(values, icon) {
   values.forEach((value) => {
@@ -20,8 +21,9 @@ export function renderDailyWeather({ daily }) {
   daily.forEach((day) => {
     let date = new Date(day.timeStamp).getDay();
     const div = template.content.cloneNode(true);
+    const icon = iconMap.get(day.iconCode);
     div.querySelector("[week-day]").textContent = weekDays[date];
-
+    // div.querySelector("[icon-day").src = `images/${icon}`;
     div.querySelector("[max-day]").textContent = day.maxTemp + "°";
     div.querySelector("[min-day]").textContent = day.minTemp + "°";
     boxes.appendChild(div);
