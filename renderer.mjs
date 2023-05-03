b86ccdd68ea0ce56cbb78af2dd0e3c3ba91b22d6
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-05-03 15:28:10 -0300

    organizei os maps e preparei para mudar o fundo

--- renderer.mjs
+++ renderer.mjs
@@ -1,5 +1,8 @@
 import { loadChart } from "./chart.mjs";
 import { getData } from "./script.mjs";
+import { weatherMap } from "./maps.mjs";
+import { iconMap } from "./maps.mjs";
+// import { backgroundMap } from "./maps.mjs";
 
 let weekDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
 let currDate = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"];
@@ -16,40 +19,7 @@ const wkday = document.querySelector("#wkday");
 const yearnmonth = document.querySelector("#yearnmonth");
 const how = document.querySelector("#how");
 
-const weatherMap = new Map();
-const iconMap = new Map();
-addIconMapping([0], "clear.svg");
-addIconMapping([1,2], "part-clear.svg");
-addIconMapping([3], "cloudy.svg");
-addIconMapping([45], "fogs.svg");
-addIconMapping([48], "fogl.svg");
-addIconMapping([51,53,56,61,63,66,80,81], "rains.svg");
-addIconMapping([55,57,65,67,82], "rainl.svg");
-addIconMapping([95], "thunderstorm.svg");
-addIconMapping([96,99], "hail.svg");
 
-addWeatherMapping([0], "Ensolarado");
-addWeatherMapping([1,2], "Parcialmente ensolarado");
-addWeatherMapping([3], "Nublado");
-addWeatherMapping([45], "Pouca neblina");
-addWeatherMapping([48], "Muita neblina");
-addWeatherMapping([51,53,56,61,63,66,80,81], "Pouca chuva");
-addWeatherMapping([55,57,65,67,82], "Muita chuva");
-addWeatherMapping([95], "Tempestade");
-addWeatherMapping([96,99], "Granizo");
-
-
-function addIconMapping(values, icon) {
-  values.forEach((value) => {
-    iconMap.set(value, icon);
-  });
-}
-
-function addWeatherMapping(values, weather) {
-  values.forEach((value) => {
-    weatherMap.set(value, weather);
-  });
-};
 
 export function renderCurrentWeather(data) {
   let date = new Date(data.daily[0].timeStamp)
