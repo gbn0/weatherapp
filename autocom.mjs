ffab435f8a486c2ea660c9967d0528f8619e4e9d
Your Name <you@example.com>
2023-05-04 19:05:22 -0300

    criei a div com as pesquisas

--- autocom.mjs
+++ autocom.mjs
@@ -1,43 +1,12 @@
 import { GetCity } from "./cities.mjs";
 
 const searchWrapper = document.querySelector(".search-box");
-const inputBox = searchWrapper.querySelector("#searchcity");
+const inputBox = searchWrapper.querySelector(".search-txt");
 const suggBox = searchWrapper.querySelector(".autocom-box");
-// if user press any key and release
-inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-        for (let i = 0; i < 4; i++) {
-            GetCity(userData, i)
-        }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
-}
+const icon = searchWrapper.querySelector(".search-btn");
+let linkTag = searchWrapper.querySelector("a");
 
-function select(element){
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = `https://www.google.com/search?q=${selectData}`;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
-}
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    }else{
-      listData = list.join('');
-    }
-    suggBox.innerHTML = listData;
-}
+// if user press any key and release
+inputBox.addEventListener('input',  () => {
+    inputBox.value = 'funciona'
+})
--- cities.mjs
+++ cities.mjs
@@ -1,12 +1,16 @@
-
-export function GetCity(c, ind){
+function GetCity(c){
   const search_term = c;
-  const sug = '';
+  let emptyArray=[];
   fetch(`https://api.teleport.org/api/cities/?search=${search_term}`)
   .then(response => response.json())
   .then(data => {
-      sug = (data['_embedded']['city:search-results'][ind]['matching_alternate_names'][0]['name'])
-    }
-);
-    return sug
+    for (let i = 0; i < 4; i++) {
+      emptyArray[i] = data['_embedded']['city:search-results'][i]['matching_alternate_names'][0]['name'];
+    }}
+    
+  ); 
+  console.log(emptyArray)
+  return emptyArray;
 }
+
+GetCity('To')
