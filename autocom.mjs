d5a0e13369dc976b7808c83e9a12a698f9a44ba7
Your Name <you@example.com>
2023-05-05 02:22:58 -0300

    Agora a api fornece o nome das cidades do mundo, fatla fazer o select

--- autocom.mjs
+++ autocom.mjs
@@ -1,12 +1,42 @@
-import { GetCity } from "./cities.mjs";
-
 const searchWrapper = document.querySelector(".search-box");
 const inputBox = searchWrapper.querySelector(".search-txt");
 const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = searchWrapper.querySelector(".search-btn");
-let linkTag = searchWrapper.querySelector("a");
+// let linkTag = searchWrapper.querySelector("a");
+const suggBoxUl = suggBox.querySelector("ul");
 
-// if user press any key and release
-inputBox.addEventListener('input',  () => {
-    inputBox.value = 'funciona'
-})
+inputBox.value = '';
+
+function GetCity() {
+    const search_term = inputBox.value;
+    if(search_term){
+        suggBox.style.display = 'block'
+    }
+    else{
+        suggBox.style.display = 'none'
+    }
+    fetch(`https://api.teleport.org/api/cities/?search=${search_term}`)
+      .then(response => response.json())
+      .then(data => {
+        let html = ""; 
+        const results = data['_embedded']['city:search-results'];
+        const numResults = results.length;
+        if (numResults === 0) {
+          suggBoxUl.innerHTML = "Nenhum resultado encontrado."; // exibe uma mensagem de erro se a lista de resultados estiver vazia
+          return;
+        }
+        for (let i = 0; i < numResults && i < 4; i++) { // define o limite superior do loop para o número real de resultados
+            const city = results[i]['matching_full_name'];
+          html += `<li>${city}</li>`;
+        }
+        suggBoxUl.innerHTML = html;
+      });
+  }
+  
+  
+inputBox.addEventListener("input", GetCity);
+
+searchWrapper.addEventListener('mouseleave', () => {
+    inputBox.value = '';
+    GetCity()
+  });
--- cities.mjs
+++ cities.mjs
@@ -1,16 +1,16 @@
-function GetCity(c){
-  const search_term = c;
-  let emptyArray=[];
-  fetch(`https://api.teleport.org/api/cities/?search=${search_term}`)
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < 4; i++) {
-      emptyArray[i] = data['_embedded']['city:search-results'][i]['matching_alternate_names'][0]['name'];
-    }}
-    
-  ); 
-  console.log(emptyArray)
-  return emptyArray;
-}
-
-GetCity('To')
+// fetch(`https://api.teleport.org/api/cities/?search=Caxias`)
+//       .then(response => response.json())
+//       .then(data => {
+//         let html = ""; 
+//         const results = data['_embedded']['city:search-results'];
+//         const numResults = results.length;
+//         if (numResults === 0) {
+//           suggBoxUl.innerHTML = "Nenhum resultado encontrado."; // exibe uma mensagem de erro se a lista de resultados estiver vazia
+//           return;
+//         }
+//         for (let i = 0; i < numResults && i < 4; i++) { // define o limite superior do loop para o número real de resultados
+//           const city = results[i]['matching_full_name'];
+//           html += `<li>${city}</li>`;
+//         }
+//         suggBoxUl.innerHTML = html;
+//       });
