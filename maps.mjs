2f9df1b61de44d3bbe7f4b7dd07297590a589ae4
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-05-08 11:29:30 -0300

    versao final

--- maps.mjs
+++ maps.mjs
@@ -11,6 +11,7 @@ addIconMapping([51,53,56,61,63,66,80,81], "rains.svg");
 addIconMapping([55,57,65,67,82], "rainl.svg");
 addIconMapping([95], "thunderstorm.svg");
 addIconMapping([96,99], "hail.svg");
+addIconMapping([71, 73, 75, 77, 85, 86], "snow.svg");
 
 addWeatherMapping([0], "Ensolarado");
 addWeatherMapping([1,2], "Parcialmente ensolarado");
@@ -19,13 +20,14 @@ addWeatherMapping([45], "Pouca neblina");
 addWeatherMapping([48], "Muita neblina");
 addWeatherMapping([51,53,56,61,63,66,80,81], "Pouca chuva");
 addWeatherMapping([55,57,65,67,82], "Muita chuva");
+addWeatherMapping([71, 73, 75, 77, 85, 86], "Nevando");
 addWeatherMapping([95], "Tempestade");
 addWeatherMapping([96,99], "Granizo");
 
 
 addBackgroundMapping([0,1,2], 'linear-gradient(to bottom,rgb(9,30,58)-40%,rgb(47,128,237)20%,rgb(45,158,224)100%)');
 addBackgroundMapping([3,45,48], 'linear-gradient(to bottom,rgb(0,0,0) -40%,rgb(75,82,107)10%,rgb(45,158,224)100%)');
-addBackgroundMapping([51,53,56,61,63,66,80,81,55,57,65,67,82,95,96,99], 'linear-gradient(to bottom, rgb(0,0,0) -30%,rgb(50,122,203) 100%,rgb(101,111,143) 101%)');
+addBackgroundMapping([51,53,56,61,63,66,71,73,75,77,80,81,85,86,55,57,65,67,82,95,96,99], 'linear-gradient(to bottom, rgb(0,0,0) -30%,rgb(50,122,203) 100%,rgb(101,111,143) 101%)');
 
 function addBackgroundMapping(values, gradient) {
   values.forEach((value) => {
